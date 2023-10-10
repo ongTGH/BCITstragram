d8a8fdd13b69277b53aa4a43e67533fa7237d449
Tommy <tommyong53@gmail.com>
2023-10-10 11:26:31 -0700

    grayscale

--- IOhandler.js
+++ IOhandler.js
@@ -9,9 +9,11 @@
  */
 
 const unzipper = require("unzipper"),
-  fs = require("fs"),
+  fs = require("fs/promises"),
+  { createReadStream, createWriteStream} = require("fs")
   PNG = require("pngjs").PNG,
   path = require("path");
+  transformStream = new PNG({});
 
 /**
  * Description: decompress file from given pathIn, write to given pathOut
@@ -20,7 +22,16 @@ const unzipper = require("unzipper"),
  * @param {string} pathOut
  * @return {promise}
  */
-const unzip = (pathIn, pathOut) => {};
+const unzip = (pathIn, pathOut) => {
+  return new Promise((resolve, reject) => {
+    createReadStream(pathIn)
+      .on("end", resolve)
+      .on("error", reject)
+    createWriteStream(pathOut)
+      .on("end", resolve)
+      .on("error", reject)
+  })
+};
 
 /**
  * Description: read all the png files from given directory and return Promise containing array of each png file path
@@ -38,7 +49,27 @@ const readDir = (dir) => {};
  * @param {string} pathProcessed
  * @return {promise}
  */
-const grayScale = (pathIn, pathOut) => {};
+const grayScale = (pathIn, pathOut) => {
+  createReadStream(pathIn)
+  .pipe(transformStream)
+  .on("parsed", function () {
+    for (var y = 0; y < this.height; y++) {
+      for (var x = 0; x < this.width; x++) {
+        var idx = (this.width * y + x) << 2;
+
+        r = this.data[idx]*0.3
+        g = this.data[idx + 1]*0.59;
+        b = this.data[idx + 2]*0.11;
+        gray = r + g + b
+
+        this.data[idx] = gray
+        this.data[idx + 1] = gray
+        this.data[idx + 2] = gray
+      }
+    }
+    this.pack().pipe(createWriteStream(pathOut));
+  });
+};
 
 module.exports = {
   unzip,
--- main.js
+++ main.js
@@ -27,31 +27,94 @@ const transformStream = new PNG({});
 //Step 5: After ALL IMAGES have SUCCESSFULLY been grayscaled, show a success message 
 //ALL ERRORS MUST SHOW IN .catch in PROMISE CHAIN
 
+// const unzip = (pathIn, pathOut) => {
+//     return new Promise((resolve, reject) => {
+//         fs.createReadStream(pathIn)
+//             .on("end", resolve)
+//             .on("error", reject)
+//     } )
+// }
+
 // [grayScale("img1.png"), grayScale("img2.png"), grayScale("img3.png")]
 //     .then(() => console.log("All images done!"))
 
-fs.createReadStream(zipFilePath)
-    .pipe(unzipper.Extract({ path: './unzipped' }));
+// fs.createReadStream(zipFilePath)
+//     .pipe(unzipper.Extract({ path: './unzipped' }));
 
 // fs.createReadStream("./unzipped/in.png")
 //     .on("data", (chunk) => console.log(chunk))
 
-fs.createReadStream("./unzipped/in.png")
-  .pipe(transformStream)
-  .on("parsed", function () {
+// fs.createReadStream("./unzipped/in.png")
+//   .pipe(transformStream)
+//   .on("parsed", function () {
+//     for (var y = 0; y < this.height; y++) {
+//       for (var x = 0; x < this.width; x++) {
+//         var idx = (this.width * y + x) << 2;
+
+//         // invert color
+//         r = this.data[idx]*0.3
+//         g = this.data[idx + 1]*0.59;
+//         b = this.data[idx + 2]*0.11;
+//         gray = r + g + b
+
+//         this.data[idx] = gray
+//         this.data[idx + 1] = gray
+//         this.data[idx + 2] = gray
+
+//       }
+//     }
+
+//     this.pack().pipe(fs.createWriteStream("out.png"));
+//   });
+
+IOhandler.grayScale("./unzipped/in.png", "out.png")
+
+//Easier Method
+
+// fs.createReadStream("./unzipped/in.png")
+//   .pipe(
+//     new PNG({
+//       colorType: 0,
+//       },
+//     )
+//   )
+//   .on("parsed", function () {
+//     this.pack().pipe(fs.createWriteStream("out.png"));
+//   });
+
+const grayScale = (pathIn, pathOut) => {
+    var fs = require("fs"),
+        PNG = require("pngjs").PNG;
+    // Streams
+    const readStream = fs.createReadStream(pathIn);
+    const writeStream = fs.createWriteStream(pathOut);
+    const pngStream = new PNG().on("parsed", function() {
+        const modifiedImage = handleGrayscale();
+        modifiedImage.pack()
+    });
+    // Connect the Streams
+    readStream
+        .on("error", (err) => console.log(err))
+        .pipe(pngStream)
+        .on("error", (err) => console.log(err))
+        .pipe(writeStream)
+        .on("error", (err) => console.log(err))
+}
+
+function handleGrayscale() {
     for (var y = 0; y < this.height; y++) {
-      for (var x = 0; x < this.width; x++) {
-        var idx = (this.width * y + x) << 2;
-
-        // invert color
-        this.data[idx] = 255 - this.data[idx];
-        this.data[idx + 1] = 255 - this.data[idx + 1];
-        this.data[idx + 2] = 255 - this.data[idx + 2];
-
-        // and reduce opacity
-        this.data[idx + 3] = this.data[idx + 3] >> 1;
-      }
+        for (var x = 0; x < this.width; x++) {
+          var idx = (this.width * y + x) << 2;
+  
+          r = this.data[idx]*0.3
+          g = this.data[idx + 1]*0.59;
+          b = this.data[idx + 2]*0.11;
+          gray = r + g + b
+  
+          this.data[idx] = gray
+          this.data[idx + 1] = gray
+          this.data[idx + 2] = gray
+        }
+        return this.data
     }
-
-    this.pack().pipe(fs.createWriteStream("out.png"));
-  });
+}
