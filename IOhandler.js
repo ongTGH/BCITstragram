a2b4b136b68ef20b164b87638da9ba1840a4415e
Tommy <tommyong53@gmail.com>
2023-10-16 23:40:02 -0700

    readdir

--- IOhandler.js
+++ IOhandler.js
@@ -15,6 +15,11 @@ const unzipper = require("unzipper"),
   path = require("path");
   transformStream = new PNG({});
 
+const AdmZip = require("adm-zip");
+
+// const zip = new AdmZip("./myfile.zip");
+// zip.extractAllTo("./unzipped", true);
+
 /**
  * Description: decompress file from given pathIn, write to given pathOut
  *
@@ -23,14 +28,15 @@ const unzipper = require("unzipper"),
  * @return {promise}
  */
 const unzip = (pathIn, pathOut) => {
-  return new Promise((resolve, reject) => {
-    createReadStream(pathIn)
-      .on("end", resolve)
-      .on("error", reject)
-    createWriteStream(pathOut)
-      .on("end", resolve)
-      .on("error", reject)
-  })
+  return new Promise(async (resolve, reject) => {
+    try {
+      const zip = new AdmZip(pathIn);
+      zip.extractAllTo(pathOut, true);
+      resolve("Extraction operation complete");
+    } catch (err) {
+      reject(err);
+    }
+  });
 };
 
 /**
@@ -39,7 +45,19 @@ const unzip = (pathIn, pathOut) => {
  * @param {string} path
  * @return {promise}
  */
-const readDir = (dir) => {};
+const readDir = (dir) => {
+  return new Promise((resolve, reject) => {
+    fs.readdir(dir, (err, files) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      const imgFiles = files.filter((file) => path.extname(file) === '.png');
+      const imgPaths = imgFiles.map((file) => path.join(dir, file));
+      resolve(imgPaths);
+    });
+  });
+};
 
 /**
  * Description: Read in png file by given pathIn,
@@ -50,24 +68,26 @@ const readDir = (dir) => {};
  * @return {promise}
  */
 const grayScale = (pathIn, pathOut) => {
-  createReadStream(pathIn)
-  .pipe(transformStream)
-  .on("parsed", function () {
-    for (var y = 0; y < this.height; y++) {
-      for (var x = 0; x < this.width; x++) {
-        var idx = (this.width * y + x) << 2;
+  return new Promise((resolve, reject) => {
+    createReadStream(pathIn)
+      .pipe(transformStream).on("parsed", function () {
+        for (let y = 0; y < this.height; y++) {
+          for (let x = 0; x < this.width; x++) {
+            const idx = (this.width * y + x) << 2;
+            const r = this.data[idx] * 0.3;
+            const g = this.data[idx + 1] * 0.59;
+            const b = this.data[idx + 2] * 0.11;
+            const gray = r + g + b;
 
-        r = this.data[idx]*0.3
-        g = this.data[idx + 1]*0.59;
-        b = this.data[idx + 2]*0.11;
-        gray = r + g + b
-
-        this.data[idx] = gray
-        this.data[idx + 1] = gray
-        this.data[idx + 2] = gray
-      }
-    }
-    this.pack().pipe(createWriteStream(pathOut));
+            this.data[idx] = gray;
+            this.data[idx + 1] = gray;
+            this.data[idx + 2] = gray;
+          }
+        }
+        this.pack().pipe(createWriteStream(pathOut)).on("close", resolve);
+      });
+    createReadStream(pathIn).on("error", reject);
+    createWriteStream(pathOut).on("error", reject);
   });
 };
 
--- main.js
+++ main.js
@@ -13,108 +13,21 @@ const IOhandler = require("./IOhandler");
 const zipFilePath = path.join(__dirname, "myfile.zip");
 const pathUnzipped = path.join(__dirname, "unzipped");
 const pathProcessed = path.join(__dirname, "grayscaled");
-const unzipper = require('unzipper');
-const fs = require('fs');
-const PNG = require("pngjs").PNG;
-const transformStream = new PNG({});
 
-// Work with promises!!! (promise.all())
-
-//Step 1: Read the zip file
-//Step 2: Unzip the zip file (.then(___))
-//Step 3: Read all png images from unzipped folder
-//Step 4: Send them to the grayscale filter function
-//Step 5: After ALL IMAGES have SUCCESSFULLY been grayscaled, show a success message 
-//ALL ERRORS MUST SHOW IN .catch in PROMISE CHAIN
-
-// const unzip = (pathIn, pathOut) => {
-//     return new Promise((resolve, reject) => {
-//         fs.createReadStream(pathIn)
-//             .on("end", resolve)
-//             .on("error", reject)
-//     } )
-// }
-
-// [grayScale("img1.png"), grayScale("img2.png"), grayScale("img3.png")]
-//     .then(() => console.log("All images done!"))
-
-// fs.createReadStream(zipFilePath)
-//     .pipe(unzipper.Extract({ path: './unzipped' }));
-
-// fs.createReadStream("./unzipped/in.png")
-//     .on("data", (chunk) => console.log(chunk))
-
-// fs.createReadStream("./unzipped/in.png")
-//   .pipe(transformStream)
-//   .on("parsed", function () {
-//     for (var y = 0; y < this.height; y++) {
-//       for (var x = 0; x < this.width; x++) {
-//         var idx = (this.width * y + x) << 2;
-
-//         // invert color
-//         r = this.data[idx]*0.3
-//         g = this.data[idx + 1]*0.59;
-//         b = this.data[idx + 2]*0.11;
-//         gray = r + g + b
-
-//         this.data[idx] = gray
-//         this.data[idx + 1] = gray
-//         this.data[idx + 2] = gray
-
-//       }
-//     }
-
-//     this.pack().pipe(fs.createWriteStream("out.png"));
-//   });
-
-IOhandler.grayScale("./unzipped/in.png", "out.png")
-
-//Easier Method
-
-// fs.createReadStream("./unzipped/in.png")
-//   .pipe(
-//     new PNG({
-//       colorType: 0,
-//       },
-//     )
-//   )
-//   .on("parsed", function () {
-//     this.pack().pipe(fs.createWriteStream("out.png"));
-//   });
-
-const grayScale = (pathIn, pathOut) => {
-    var fs = require("fs"),
-        PNG = require("pngjs").PNG;
-    // Streams
-    const readStream = fs.createReadStream(pathIn);
-    const writeStream = fs.createWriteStream(pathOut);
-    const pngStream = new PNG().on("parsed", function() {
-        const modifiedImage = handleGrayscale();
-        modifiedImage.pack()
-    });
-    // Connect the Streams
-    readStream
-        .on("error", (err) => console.log(err))
-        .pipe(pngStream)
-        .on("error", (err) => console.log(err))
-        .pipe(writeStream)
-        .on("error", (err) => console.log(err))
+function main() {
+  IOhandler.unzip(zipFilePath, pathUnzipped)
+    .then((result) => {
+      console.log(result);
+      return IOhandler.readDir(pathUnzipped);
+    })
+    .then((imgFiles) => {
+      const grayscalePromises = imgFiles.map((pathUnzipped) => {
+        return IOhandler.grayScale(pathUnzipped, pathProcessed)
+      });
+      return Promise.all(grayscalePromises);
+    })
 }
 
-function handleGrayscale() {
-    for (var y = 0; y < this.height; y++) {
-        for (var x = 0; x < this.width; x++) {
-          var idx = (this.width * y + x) << 2;
-  
-          r = this.data[idx]*0.3
-          g = this.data[idx + 1]*0.59;
-          b = this.data[idx + 2]*0.11;
-          gray = r + g + b
-  
-          this.data[idx] = gray
-          this.data[idx + 1] = gray
-          this.data[idx + 2] = gray
-        }
-        return this.data
-    }
-}
+main();
+
+// IOhandler.grayScale("./unzipped/in.png", "sd.png")
